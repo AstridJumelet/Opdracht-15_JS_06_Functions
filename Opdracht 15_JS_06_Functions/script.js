84ce12e2939a266290f926f1a854c97a9b810be7
Astrid Jumelet <89905425+AstridJumelet@users.noreply.github.com>
2021-11-03 18:43:33 +0100

    Add files via upload

---
+++ Opdracht 15_JS_06_Functions/script.js
@@ -0,0 +1,26 @@
+//opdracht pizza maken stap 3: geen idee. Ik heb JS met Node laten runnen op oude tekst in dit bestand. Daarna die tekst verwijderd
+//opdracht pizza maken stap 4-5
+//naam voor de functie wordt maakPizza
+//stappen bij de functie pizzamaken worden: smeer het deeg in met tomatensaus, beleg met ingredienten, zet in de oven
+//schrijf de function
+//function maakPizza(smeer het deeg in met tomatensaus, beleg met ingredienten, zet in de oven)
+//stap 7 - nee, dit werkt nog niet, want je hebt nog geen code eraan gekoppeld om te kunnen gebruiken
+//dus code toevoegen met de {code hier} en daarna de functie aanroepen 
+function maakPizza() {
+    var maakPizza = prompt('smeer het deef in met tomatensaus', 'oK');
+    var maakPizza = prompt('beleg met ingredienten', 'OK');
+    var maakPizza = prompt('zet in de oven', 'OK');
+}
+maakPizza(); //aanroepen functie
+//opdracht stap 11-12
+function maakSushi() {
+    var maakSushi = prompt('maak de sushi', 'OK');
+}
+maakSushi();
+//opdracht stap 13 -  Code voor 3x pizza maken en 2x sushi:
+//bovenstaande aanroepen moet je dan volgens mij verwijderen, en het volgende invoeren: 
+maakPizza();
+maakPizza();
+maakPizza();
+maakSushi();
+maakSushi();
